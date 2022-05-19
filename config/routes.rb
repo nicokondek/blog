552503596cc33e6890e2a0072627422f10f8e925
nicolas kondek <nicolas.kondek@gmail.com>
2022-05-19 15:23:26 -0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root "articles#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+ resources :articles do
+   resources :comments
+ end
+end
